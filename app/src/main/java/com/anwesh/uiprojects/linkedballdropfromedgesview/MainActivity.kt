a61af8cd38b9d7137e90ebbb13c103dcc2770fa7
Anwesh43 <anweshthecool0@gmail.com>
2020-08-28 01:57:29 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedballdropfromedgesview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedballdropfromedgesview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedballdropfromedgesview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.balldropfromedgesview.BallDropFromEdgesView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        BallDropFromEdgesView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }

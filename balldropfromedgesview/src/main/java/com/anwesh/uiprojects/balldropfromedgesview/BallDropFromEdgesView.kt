a35e3d81242e93522ccecf9797ff0cb22d65dcee
Anwesh43 <anweshthecool0@gmail.com>
2020-08-28 01:50:12 +0530

    adding BallDropFromEdge class

--- balldropfromedgesview/src/main/java/com/anwesh/uiprojects/balldropfromedgesview/BallDropFromEdgesView.kt
+++ balldropfromedgesview/src/main/java/com/anwesh/uiprojects/balldropfromedgesview/BallDropFromEdgesView.kt
@@ -150,4 +150,27 @@ class BallDropFromEdgesView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BallDropFromEdge(var i : Int) {
+
+        private var curr : BDFENode = BDFENode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }

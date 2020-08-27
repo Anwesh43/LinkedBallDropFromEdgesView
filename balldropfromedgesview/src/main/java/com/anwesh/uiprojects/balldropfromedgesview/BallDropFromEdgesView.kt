2e7a7b37c863010495370f5c8991709e10513e2b
Anwesh43 <anweshthecool0@gmail.com>
2020-08-28 01:47:57 +0530

    adding BDFENode class

--- balldropfromedgesview/src/main/java/com/anwesh/uiprojects/balldropfromedgesview/BallDropFromEdgesView.kt
+++ balldropfromedgesview/src/main/java/com/anwesh/uiprojects/balldropfromedgesview/BallDropFromEdgesView.kt
@@ -109,4 +109,45 @@ class BallDropFromEdgesView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BDFENode(var i : Int, val state : State = State()) {
+
+        private var next : BDFENode? = null
+        private var prev : BDFENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BDFENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBDFENode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BDFENode {
+            var curr : BDFENode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
